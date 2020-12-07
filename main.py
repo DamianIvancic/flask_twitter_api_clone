285fba2878b008da1869e34cc7aa043a6bb33eb0
Damian <damian@exordiumgames.com>
2020-12-07 15:46:46 +0100

    modified main

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from api import api as api_blueprint
 import os
-import click
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
